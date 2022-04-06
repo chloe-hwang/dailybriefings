0b9c22eb7635e5f046c95c411467a8e36defa089
Chloe Hwang <ch1248@georgetown.edu>
2022-04-06 17:59:33 -0400

    added notes to init file

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 APP_ENV = os.getenv("APP_ENV", default="development") # use "production" on a remote server
+
+#development env: developing it locally on our computer 
+#app_env can be updated via env variables on the server 
+    #use "production" on a remote, user-facing server 
+#helps detect which env running the app from 
